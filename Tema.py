70d78eb4bf3d6236edba0e209ebe307651b5c343
Alex Cuenca M <maxpowerxd8y9@gmail.com>
2020-10-30 10:43:07 -0500

    Modifiacion de CRUD

--- Tema.py
+++ Tema.py
@@ -4,40 +4,35 @@ class Tema:
     def __init__(self):
         self.__conexion = mysql.connector.connect(host="localhost",user="root",password="test", db="troubleshooting")        
     
-    def select_all(self):
+    def select_all(self):        
         result = [('Id','Descripcion')]        
         cursor = self.__conexion.cursor()
         cursor.execute("select codigo,descripcion from tema")        
         for codigo,descripcion in cursor.fetchall():            
-            result.append([codigo,descripcion])                
-        self.__conexion.close()
-
+            result.append([codigo,descripcion])                        
         return result
     
     def update_tema(self,id,descripcion):                
-        cursor = self.__conexion.cursor()
+        cursor = self.__conexion.cursor()        
         sql = "UPDATE tema SET descripcion=%s WHERE codigo=%s"        
         cursor.execute(sql,(descripcion,id) )
-        self.__conexion.commit()
-        self.__conexion.close()        
+        self.__conexion.commit()        
         return "%d record(s) affected" % cursor.rowcount
     
     def insert_tema(self,descripcion):
         cursor = self.__conexion.cursor()
         cursor.execute("insert into tema(descripcion,eliminado) values(%s, %s)",(descripcion,0))        
-        self.__conexion.commit()
-        self.__conexion.close()
+        self.__conexion.commit()        
         return '%d rows inserted' % cursor.rowcount
     
-    def delete_tema(self, id):            
+    def delete_tema(self, ide):                  
         cursor = self.__conexion.cursor()
-        sql = "DELETE FROM tema WHERE id=%s"        
-        cursor.execute(sql,id)
-        self.__conexion.commit()
-        self.__conexion.close()
+        sql = "DELETE FROM tema WHERE codigo=%s" % ide        
+        cursor.execute(sql)
+        self.__conexion.commit()        
 
         return "%d record(s) deleted" % cursor.rowcount
     
 
-    if __name__ == "__main__":
+    if __name__ == "__main__":                
         print("Hey, no estás en main xd")
